bdcf880d63fc870edd75e4d64c09f5e81d2eded4
Jorabek <jorabek@olcha.uz>
2023-04-14 16:35:28 +0500

    sql;

---
+++ db.sql
@@ -0,0 +1,15 @@
+create database oop;
+create table users(
+                      id int(11) not null auto_increment,
+                      name varchar(255) not null,
+                      email varchar(255) not null,
+                      password varchar(255) not null,
+                      primary key(id)
+);
+INSERT INTO users (name,email,password) VALUES ('Jorabe','kemail','apassword');
+UPDATE users SET name = 'Jorabek', email = 'j.a.y', password = 'asdasd' ,age = '12' WHERE id = 1;
+create table companies(
+                          id int(11) not null auto_increment,
+                          name varchar(255) not null,
+                          primary key(id)
+);
